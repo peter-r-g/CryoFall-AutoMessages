5e3e322390275b23ecb7885b55eda8a2404e01b7
Peter <petergorman11@gmail.com>
2021-05-11 16:30:12 -0400

    Add mod files

---
+++ Scripts/AutoMessages/AutoMessagesSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace CryoFall.AutoMessages
+{
+    public class AutoMessagesSettings
+    {
+        /// <summary>
+        /// Whether or not the messages should be displayed in sequential order or random.
+        /// </summary>
+        public bool InSequentialOrder = true;
+        /// <summary>
+        /// Whether or not a prefix should be placed before a message or not.
+        /// </summary>
+        public bool IncludePrefix = true;
+        /// <summary>
+        /// Whether or not faces and some emojis should be parse automatically. Example: :) -> 😃
+        /// </summary>
+        public bool ParseEmojis = true;
+
+        /// <summary>
+        /// The time in seconds between messages.
+        /// </summary>
+        public double DelayBetweenMessages = 60;
+
+        /// <summary>
+        /// If IncludePrefix is true, this will be the prefix placed before every message (A space (" ") is already placed after the prefix).
+        /// </summary>
+        public string Prefix = "[SERVER]";
+        /// <summary>
+        /// This will be all of the messages you want to be broadcasted to all players.
+        /// </summary>
+        public string[] Messages = new string[]
+        {
+            "This is an automated message, these and other settings can be edited in Data/Mods/AutoMessages.json :)"
+        };
+    }
+}
---
+++ Scripts/AutoMessages/BootstrapperAutoMessager.cs
@@ -0,0 +1,89 @@
+﻿namespace CryoFall.AutoMessages
+{
+    using AtomicTorch.CBND.CoreMod.Bootstrappers;
+    using AtomicTorch.CBND.CoreMod.Systems.Chat;
+    using AtomicTorch.CBND.CoreMod.Systems.EmojiSystem;
+    using AtomicTorch.CBND.CoreMod.Systems.ServerTimers;
+    using AtomicTorch.CBND.GameApi.Data;
+    using AtomicTorch.CBND.GameApi.Scripting;
+    using AtomicTorch.GameEngine.Common.Helpers;
+    using System;
+
+    [PrepareOrder(afterType: typeof(BootstrapperServerCore))]
+    public class BootstrapperAutoMessages : BaseBootstrapper
+    {
+        public static string SettingsFileName = "Data/Mods/AutoMessages.json";
+
+        public AutoMessagesSettings Settings;
+
+        private int currentOrder;
+
+        public override void ServerInitialize(IServerConfiguration serverConfiguration)
+        {
+            LoadSettings(SettingsFileName);
+            if (Settings.InSequentialOrder)
+            {
+                currentOrder = -1;
+            }
+
+            SendAutomatedMessage();
+        }
+
+        private void LoadSettings(string fileName)
+        {
+            // Check if settings file exists.
+            // If it does, load it. Other wise create new settings and save it.
+
+            Settings = new AutoMessagesSettings();
+        }
+
+        /// <summary>
+        /// Just a stub for when I find methods to save/load data.
+        /// </summary>
+        /// <param name="fileName"></param>
+        private void SaveSettings(string fileName)
+        {
+        }
+
+        private string NextMessage()
+        {
+            if (Settings.InSequentialOrder)
+            {
+                currentOrder++;
+                if (currentOrder >= Settings.Messages.Length)
+                {
+                    currentOrder = 0;
+                }
+
+                return Settings.Messages[currentOrder];
+            }
+            else
+            {
+                return Settings.Messages[RandomHelper.Next(0, Settings.Messages.Length)];
+            }
+        }
+
+        private void SendAutomatedMessage()
+        {
+            try
+            {
+                string message = Settings.IncludePrefix ? $"{Settings.Prefix} " : "";
+                message += NextMessage();
+                if (Settings.ParseEmojis)
+                {
+                    message = EmojiHelper.ReplaceAsciiToUnicodeEmoji(message);
+                }
+
+                ChatSystem.ServerSendGlobalServiceMessage(message);
+            }
+            catch (NullReferenceException)
+            {
+                // Chat system isn't ready yet, try again next server frame.
+                ServerTimersSystem.AddAction(0, SendAutomatedMessage);
+                return;
+            }
+
+            ServerTimersSystem.AddAction(Settings.DelayBetweenMessages, SendAutomatedMessage);
+        }
+    }
+}
